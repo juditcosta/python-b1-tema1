16f058bc55a215f3877936fdbaeb6d4b5f301e10
juditcosta <161055645+juditcosta@users.noreply.github.com>
2026-06-16 20:11:48 +0200

    Add count_vowels function to count vowels
    Implemented the count_vowels function to count vowels in a given string.

--- 1a/ej1a4.py
+++ 1a/ej1a4.py
@@ -34,8 +34,12 @@ Exemple:
 '''
 
 def count_vowels(text_chain:str):
-    # Write here your code
-    pass
+    count = 0
+    vocals = "AaEeIiOoUu"
+    for i in text_chain:
+        if i in vocals:
+            count = count + 1
+    return count
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
